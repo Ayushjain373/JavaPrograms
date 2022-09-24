28fc0cf9fec89872fa19c8577d9d566e164416d6
sumitchugh55 <schugh867@gmail.com>
2022-09-24 18:38:10 +0530

    First Commit

---
+++ Test.java
@@ -0,0 +1,26 @@
+package sumit;
+
+public class Test{
+	static int d=60;//static variable
+	
+	
+	void display()
+	{
+		int t=70;//Local Variable
+		System.out.println(t);
+	}
+	
+	
+	
+public static void main(String[] args) {
+	
+	int f=500;
+	System.out.println(f);
+	Test t1=new Test();
+	t1.display();
+	System.out.println(d);
+	
+	
+
+}
+}
