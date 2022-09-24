a839aedf10dc6a26066d385bd99cc8ac69be5d59
sumitchugh55 <83135527+sumitchugh55@users.noreply.github.com>
2022-09-24 19:01:39 +0530

    Add files via upload

---
+++ VarKeyword.java
@@ -0,0 +1,16 @@
+package sumit;
+
+public class VarKeyword {
+	
+	
+	public static void main(String[] args) {
+		var t=98;
+		var f=89.0;
+		var s="Sumit chugh";
+		System.out.println(t);
+		System.out.println(f);
+		System.out.println(s);
+		
+	}
+
+}
